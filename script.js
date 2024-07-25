1d47f55d07d94d7332ef2b69ece48b4cc34df00b
samithd03 <samithd03@gmail.com>
2024-07-25 20:40:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+
+keepAlive();
+const monitor = new Monitor({
+    website: '',
+    title: 'NAME',
+    interval: 2
+});
+
+monitor.on('up', (res) => console.log(`${res.website} its on.`));
+monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} has stopped.`) );
+monitor.on('error', (error) => console.log(error));
